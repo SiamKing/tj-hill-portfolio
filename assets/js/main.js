cf482b563d51dc90b9f5e4e707ba7ffe56f149c3
SiamKing <thesiameseelectric@gmail.com>
2017-06-01 17:09:55 -0700

    jquery for navbar active and scrolling works

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,44 +1,29 @@
 $(() => {
-   var scroll_start = 0;
-   var about = $('#about');
-   var offset = about.offset();
-  //  offset.top -= 10;
-    if (about.length){
-   $(document).scroll(() => {
+  var scroll_start = 0;
+  var about = $('#about');
+  var offset = about.offset();
+  offset.top -= 10;;
+  if (about.length){
+    $(document).scroll(function() {
       scroll_start = $(this).scrollTop();
       if(scroll_start > offset.top) {
-          $(".navbar-default").css({'background-color': '#fff'});
-          $("nav a").css('color', 'black');
-       } else {
-          $('.navbar-default').css({'background-color': 'transparent'});
-          $("nav a").css({'color': 'white', 'background-color': 'transparent'});
-       }
-   });
+        $(".navbar-default").css({'background-color': '#fff', 'opacity': '1'});
+        $("nav a").css('color', 'black');
+      } else {
+        $('.navbar-default').css({'background-color': 'transparent'});
+        $("nav a").css({'color': 'white', 'background-color': 'transparent'});
+      }
+    });
+  }
 
-   $("#nav-home").click(() => {
-     $.scrollTo("#top", 800);
-   });
+  $("ul.nav li").click(function() {
+    var anchor = $(this).find('a').attr('href')
 
-   $("#about-me").click(() => {
-     $.scrollTo("#about", 800);
-     $(".navbar-default").css({'background-color': '#fff'});
-     $("nav a").css('color', 'black');
-   });
+    $.scrollTo(anchor, 800)
+    // remove classes from all
+    $("nav ul li").removeClass("active");
+    // add class to the one we clicked
+    $(this).addClass("active");
+  });
 
-   $("#nav-skills").click(() => {
-     $.scrollTo("#skills", 800);
-     $(".navbar-default").css({'background-color': '#fff'});
-     $("nav a").css('color', 'black');
-   });
-
-
-
-
-   $("ul li").click(() => {
-      // remove classes from all
-      $("ul li").removeClass("active");
-      // add class to the one we clicked
-      $(this).addClass("active");
-   });
-    }
 });
