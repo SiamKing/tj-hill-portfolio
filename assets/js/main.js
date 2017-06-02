754b83ba0b6aed145f45425b0154a839321ad5e1
SiamKing <thesiameseelectric@gmail.com>
2017-06-01 20:24:49 -0700

    fixed scroll - it was reverting back to home in between each anchor

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,25 +19,25 @@ $(function() {
   $(document).on("scroll", onScroll);
 
 
-    $('a[href^="#"]').on('click', function (e) {
-        e.preventDefault();
-        $(document).off("scroll");
-
-        $('a').each(function () {
-            $(this).removeClass('active');
-        })
-        $(this).addClass('active');
-
-        var target = this.hash,
-            menu = target;
-        $target = $(target);
-        $('html, body').stop().animate({
-            'scrollTop': $target.offset().top+2
-        }, 500, 'swing', function () {
-            window.location.hash = target;
-            $(document).on("scroll", onScroll);
-        });
-    });
+    // $('a[href^="#"]').on('click', function (e) {
+    //     e.preventDefault();
+    //     $(document).off("scroll");
+    //
+    //     $('a').each(function () {
+    //         $(this).removeClass('active');
+    //     })
+    //     $(this).addClass('active');
+    //
+    //     var target = this.hash,
+    //         menu = target;
+    //     $target = $(target);
+    //     $('html, body').stop().animate({
+    //         'scrollTop': $target.offset().top+2
+    //     }, 500, 'swing', function () {
+    //         window.location.hash = target;
+    //         $(document).on("scroll", onScroll);
+    //     });
+    // });
 
   $("ul.nav li").click(function() {
     var anchor = $(this).find('a').attr('href')
@@ -55,12 +55,12 @@ $(function() {
 
 function onScroll(event){
     var scrollPos = $(document).scrollTop();
-    $('li a').each(function () {
+    $('nav ul li').each(function () {
         var currLink = $(this);
-        var refElement = $(currLink.attr("href"));
-        console.log(refElement.position().top)
-        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-            $('navbar ul li a').removeClass("active");
+        var refElement = $(currLink.find('a').attr("href"));
+
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() + 50 > scrollPos) {
+            $('nav ul li').removeClass("active");
             currLink.addClass("active");
         }
         else{
